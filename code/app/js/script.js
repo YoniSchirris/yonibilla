52258b5b4accae964dc42d2a50a8417521de3a5d
YoniSchirris <yschirris@gmail.com>
2018-03-26 17:58:13 +0200

    Whole page responsive, form submit is as functional as can get at this time, the year is now on top of the sliding div

--- code/app/js/script.js
+++ code/app/js/script.js
@@ -21,6 +21,8 @@ function callbackFunc() {
     for (var i = 0; i < items.length; i++) {
         if (isElementInViewport(items[i])) {
             items[i].classList.add("in-view");
+
+
         }
     }
 }
@@ -29,3 +31,44 @@ function callbackFunc() {
 // We run the code whenever the user scrolls and when the website is loaded
 window.addEventListener("load", callbackFunc);
 window.addEventListener("scroll", callbackFunc);
+
+
+
+
+// Handles the form submission
+
+$(document).ready(function() {
+
+    $('#info-request').click(function () {
+        if ($('#subscriber_email').val().includes("@")){
+            console.log("This is what is being submitted and could be thrown towards the back-end");
+            console.log($('#subscriber_email').val())
+    } else {
+            alert("That doesn't look like an e-mail address. Please fill in your e-mail address");
+            return false;
+
+
+        }
+
+
+
+    })
+
+})
+
+
+
+// AJAX Post request
+
+function postNameAndEmailToServer() {
+    $.post("usabilla_test_post.asp",
+        {
+            name: $('#subscriber_name').val(),
+            city: $('#subscriber_email').val()
+        },
+
+        //Below shows responses for whenever we really start testing
+        function (data, status) {
+            alert("Data: " + data + "\nStatus: " + status);
+        });
+}
