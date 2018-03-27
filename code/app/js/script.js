6bcf3260b3aa74aae0da531b86957d56101deb18
YoniSchirris <yschirris@gmail.com>
2018-03-27 16:00:44 +0200

    GULP build automation finalized with css js and image optimization and build functionality. Added future product cards that pop out. Fully fixed subscribe button functionality

--- code/app/js/script.js
+++ code/app/js/script.js
@@ -1,4 +1,3 @@
-
 // Checks if a given element is in the user's screen
 function isElementInViewport(el) {
     var rect = el.getBoundingClientRect();
@@ -12,7 +11,7 @@ function isElementInViewport(el) {
 
 
 // Define a list of all list items
-var items = document.querySelectorAll(".timeline li");
+var items = document.querySelectorAll(".timeline li, div.future-feature");
 
 
 // This function is run on every load and scroll, throws the timeline to "iSElementInViewport"
@@ -34,41 +33,58 @@ window.addEventListener("scroll", callbackFunc);
 
 
 
-
 // Handles the form submission
 
 $(document).ready(function() {
 
     $('#info-request').click(function () {
         if ($('#subscriber_email').val().includes("@")){
+            //$('#info-request').pre
             console.log("This is what is being submitted and could be thrown towards the back-end");
+            console.log($('#subscriber_name').val());
             console.log($('#subscriber_email').val())
-    } else {
+            console.log("Or, in AJAX form:");
+            console.log("usabilla_test_post.asp, {name:" + $('#subscriber_name').val() + ", email: " + $('#subscriber_email').val() + "}");
+            window.open(window.location.href.split('?')[0] + "?subscription-success?name=" + $('#subscriber_name').val(), '_self');
+
+
+        } else {
             alert("That doesn't look like an e-mail address. Please fill in your e-mail address");
             return false;
-
-
         }
-
-
-
     })
-
 })
 
 
-
-// AJAX Post request
+// AJAX Post request. This is currently not run as we don't have a server to communicate with.
 
 function postNameAndEmailToServer() {
+
+        // The actual data that's posted, then awaits a response
     $.post("usabilla_test_post.asp",
         {
             name: $('#subscriber_name').val(),
             city: $('#subscriber_email').val()
         },
 
-        //Below shows responses for whenever we really start testing
+        // Below shows responses for whenever we really start testing
         function (data, status) {
             alert("Data: " + data + "\nStatus: " + status);
         });
 }
+
+
+// Shows the success box if the argument is given to the web address
+
+if (window.location.href.indexOf("?subscription-success") == -1){
+    $('#successful-signup-message').hide();
+    // The following checks if there's something behind name=
+} else if(window.location.href.split("name=")[1]){
+    var split_url = window.location.href.split("name=");
+    var full_name = split_url[1];
+    var first_name = full_name.split("%20")[0];
+    $('#successfull-signup-text').prepend("Thank you " + first_name + "! ");
+}
+
+
+
--- code/gulpfile.js
+++ code/gulpfile.js
@@ -2,6 +2,22 @@ var gulp = require('gulp');
 
 var sass = require('gulp-sass');
 
+var useref = require('gulp-useref');
+
+var uglify = require('gulp-uglify');
+
+var cssnano = require('gulp-cssnano');
+
+var cache = require('gulp-cache');
+
+var runSequence = require('run-sequence');
+
+var del = require('del');
+
+var imagemin = require('gulp-imagemin');
+
+var gulpIf = require('gulp-if');
+
 var browserSync = require('browser-sync').create();
 
 
@@ -23,10 +39,60 @@ gulp.task('watch', ['browserSync', 'sass'], function (){
     gulp.watch('app/*.html', browserSync.reload);
     gulp.watch('app/js/**/*.js', browserSync.reload);
 });
+
+// gulp.task('default', function (callback) {
+//     runSequence(['sass','browserSync', 'watch'],
+//         callback
+//     )
+// })
+
 gulp.task('browserSync', function() {
     browserSync.init({
         server: {
             baseDir: 'app'
         },
     })
+})
+
+
+gulp.task('useref', function(){
+    return gulp.src('app/*.html')
+        .pipe(useref())
+        // Minifies only if it's a JavaScript file
+        .pipe(gulpIf('*.js', uglify()))
+        .pipe(gulp.dest('dist'))
+});
+
+gulp.task('useref', function(){
+    return gulp.src('app/*.html')
+        .pipe(useref())
+        .pipe(gulpIf('*.js', uglify()))
+        // Minifies only if it's a CSS file
+        .pipe(gulpIf('*.css', cssnano()))
+        .pipe(gulp.dest('dist'))
+});
+
+gulp.task('images', function(){
+    return gulp.src('app/images/**/*.+(png|jpg|jpeg|gif|svg)')
+    // Caching images that ran through imagemin
+        .pipe(cache(imagemin({
+            interlaced: true
+        })))
+        .pipe(gulp.dest('dist/images'))
+});
+
+gulp.task('fonts', function() {
+    return gulp.src('app/fonts/**/*')
+        .pipe(gulp.dest('dist/fonts'))
+})
+
+gulp.task('clean:dist', function() {
+    return del.sync('dist');
+})
+
+gulp.task('build', function (callback) {
+    runSequence('clean:dist',
+        ['sass', 'useref', 'images', 'fonts'],
+        callback
+    )
 })
